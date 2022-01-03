251a581fb10523e550bd7e59a1f5610b9b65e9d7
Robin van Maasakker <robin@qubiqx.com>
2022-01-03 10:02:21 +0100

    Update

--- config/filament.php
+++ config/filament.php
@@ -105,7 +105,6 @@ return [
         'namespace' => 'App\\Filament\\Widgets',
         'path' => app_path('Filament/Widgets'),
         'register' => [
-            Widgets\AccountWidget::class,
         ],
     ],
 
