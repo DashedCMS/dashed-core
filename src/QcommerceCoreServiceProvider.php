bef3504d44bdeffcb038a3cb17550641470531cd
Robin van Maasakker <robin@qubiqx.com>
2022-04-03 16:17:26 +0200

    Update

--- src/QcommerceCoreServiceProvider.php
+++ src/QcommerceCoreServiceProvider.php
@@ -73,12 +73,15 @@ class QcommerceCoreServiceProvider extends PluginServiceProvider
             ]);
     }
 
-    protected function getStyles(): array
-    {
-        return array_merge(parent::getStyles(), [
-            'qcommerce-core' => str_replace('/vendor/qubiqx/qcommerce-core/src', '', str_replace('/packages/qubiqx/qcommerce-core/src', '', __DIR__)) . '/vendor/qubiqx/qcommerce-core/resources/dist/css/qcommerce-core123.css',
-        ]);
-    }
+    protected array $scripts = [
+        'my-package-scripts' => __DIR__ . '/../dist/app.js',
+    ];
+//    protected function getStyles(): array
+//    {
+//        return array_merge(parent::getStyles(), [
+//            'qcommerce-core' => str_replace('/vendor/qubiqx/qcommerce-core/src', '', str_replace('/packages/qubiqx/qcommerce-core/src', '', __DIR__)) . '/vendor/qubiqx/qcommerce-core/resources/dist/css/qcommerce-core.css',
+//        ]);
+//    }
 
     protected function getPages(): array
     {
