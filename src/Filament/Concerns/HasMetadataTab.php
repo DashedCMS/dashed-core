3ec8a411f7b82d449bb5bf3bffc6faf722886e6f
MilanVrenssen1 <milanvrenssen@gmail.com>
2022-10-21 15:50:15 +0200

    changes meta data

--- src/Filament/Concerns/HasMetadataTab.php
+++ src/Filament/Concerns/HasMetadataTab.php
@@ -11,8 +11,41 @@ use Filament\Forms\Components\FileUpload;
 
 trait HasMetadataTab
 {
-    protected static function metadataTab(): Tab
+    protected static function metadataTab(): array
     {
+        return [
+                        TextInput::make('title')
+                            ->label('Meta titel')
+                            ->nullable()
+                            ->minLength(5)
+                            ->maxLength(70)
+                            ->rules([
+                                'nullable',
+                                'min:5',
+                                'max:70',
+                            ]),
+                        Textarea::make('description')
+                            ->label('Meta omschrijving')
+                            ->nullable()
+                            ->minLength(5)
+                            ->maxLength(170)
+                            ->rows(2)
+                            ->rules([
+                                'nullable',
+                                'min:5',
+                                'max:170',
+                            ]),
+                        FileUpload::make('image')
+                            ->label('Meta afbeelding')
+                            ->directory('qcommerce/metadata')
+                            ->image()
+                            ->helperText('De beste afmeting is 1200x630 pixels'),
+//                        TextInput::make('canonical_url')
+//                            ->label('Meta canonical URL'),
+                        Toggle::make('noindex')
+                            ->label('Pagina niet indexeren'),
+                    ];
+
         return Tab::make('Metadata')
             ->schema([
                 Fieldset::make('Metadata')
