254adf379ddf00616ab82b85240004db2bba71ab
Robin van Maasakker <robin@qubiqx.com>
2022-10-25 13:33:37 +0200

    Update

--- src/Filament/Resources/RedirectResource.php
+++ src/Filament/Resources/RedirectResource.php
@@ -44,7 +44,8 @@ class RedirectResource extends Resource
                         '302' => 'Tijdelijke redirect',
                     ]),
                 Forms\Components\DatePicker::make('delete_redirect_after')
-                    ->label('Verwijder redirect na een datum'),
+                    ->label('Verwijder redirect na een datum')
+                    ->default(now()->addMonths(3)),
             ]);
     }
 
@@ -53,12 +54,12 @@ class RedirectResource extends Resource
         return $table
             ->columns([
                 TextColumn::make('from')
-                    ->url(fn ($record) => url($record->from))
+                    ->url(fn($record) => url($record->from))
                     ->openUrlInNewTab()
                     ->label('Oude URL')
                     ->searchable(),
                 TextColumn::make('to')
-                    ->url(fn ($record) => $record->to)
+                    ->url(fn($record) => $record->to)
                     ->openUrlInNewTab()
                     ->label('Nieuwe URL')
                     ->searchable(),
@@ -66,7 +67,7 @@ class RedirectResource extends Resource
                     ->label('Soort redirect'),
                 TextColumn::make('delete_redirect_after')
                     ->label('Delete redirect na')
-                    ->getStateUsing(fn ($record) => $record->delete_redirect_after ? $record->delete_redirect_after->format('d-m-Y') : 'Niet verwijderen'),
+                    ->getStateUsing(fn($record) => $record->delete_redirect_after ? $record->delete_redirect_after->format('d-m-Y') : 'Niet verwijderen'),
             ])
             ->filters([
                 //
