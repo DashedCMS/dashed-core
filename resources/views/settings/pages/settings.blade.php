b809722e56371c114f6524d446196f02457c8f5b
MilanVrenssen1 <milanvrenssen@gmail.com>
2022-10-26 12:32:56 +0200

    update

--- resources/views/settings/pages/settings.blade.php
+++ resources/views/settings/pages/settings.blade.php
@@ -6,30 +6,26 @@
             @foreach(cms()->builder('settingPages') as $settingPage)
                 <li class="col-span-1 flex shadow-sm rounded-md">
                     <div
-                        class="flex-shrink-0 flex items-center justify-center w-16 bg-primary-600 text-white text-sm font-medium rounded-l-md">
-                        <x-dynamic-component :component="'heroicon-o-' . $settingPage['icon']"
-                                             class="w-6 h-6"></x-dynamic-component>
+                        class="col-span-1 flex items-center justify-center w-16 bg-primary-600 text-white text-sm font-medium rounded-l-md">
+                        <a class="col-span-1 flex"
+                           href="{{ $settingPage['page']::getUrl() }}">
+                            <x-dynamic-component :component="'heroicon-o-' . $settingPage['icon']"
+                                                 class="w-6 h-6"></x-dynamic-component>
+                        </a>
                     </div>
                     <div
-                        class="flex-1 flex items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md">
-                        <div class="flex-1 px-4 py-2 text-sm"><a
-                                href="{{ $settingPage['page']::getUrl() }}"
-                                class="text-gray-900 font-medium hover:text-gray-600">
-                                {{ $settingPage['name'] }}
-                            </a>
-                            <p class="text-gray-500">{{ $settingPage['description'] }}</p>
-                        </div>
-                        <div class="flex-shrink-0 pr-2">
-                            <a href="{{ $settingPage['page']::getUrl() }}"
-                               class="w-8 h-8 bg-white inline-flex items-center justify-center text-gray-400 rounded-full bg-transparent hover:text-gray-500 focus:outline-none ring-2 ring-offset-2 ring-primary-500 focus:ring-secondary-500"><span
-                                    class="sr-only">Open optie</span>
-                                <svg fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                     xmlns="http://www.w3.org/2000/svg" class="w-5 h-5">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                          d="M13 5l7 7-7 7M5 5l7 7-7 7"></path>
-                                </svg>
-                            </a>
-                        </div>
+                        class="flex-1 items-center justify-between border-t border-r border-b border-gray-200 bg-white rounded-r-md">
+                        <a class="col-span-1 flex"
+                           href="{{ $settingPage['page']::getUrl() }}">
+                            <div class="flex-1 px-4 py-2 text-sm">
+                                <h3
+                                    class="text-gray-900 font-medium hover:text-gray-600">
+                                    {{ $settingPage['name'] }}
+                                </h3>
+                                <p class="text-gray-500">{{ $settingPage['description'] }}</p>
+                            </div>
+
+                        </a>
                     </div>
                 </li>
             @endforeach
