b21b2f8aad498d98c37b56a46945bcfa17c1ced1
Robin van Maasakker <robin@qubiqx.com>
2023-07-31 19:56:31 +0200

    Update

--- routes/frontend.php
+++ routes/frontend.php
@@ -21,32 +21,30 @@ Route::group(
     function () {
         //Auth routes
 
-        if (!app()->runningInConsole()) {
-            Route::get('/' . Translation::get('logout-slug', 'slug', 'logout'), [AuthController::class, 'logout'])->name('qcommerce.frontend.auth.logout');
-            Route::group([
-                'middleware' => [GuestMiddleware::class],
-            ], function () {
-                Route::get('/' . Translation::get('login-slug', 'slug', 'login'), [AuthController::class, 'login'])->name('qcommerce.frontend.auth.login');
-                Route::post('/' . Translation::get('login-slug', 'slug', 'login'), [AuthController::class, 'loginPost'])->name('qcommerce.frontend.auth.login.post');
-                Route::post('/' . Translation::get('register-slug', 'slug', 'register'), [AuthController::class, 'login'])->name('qcommerce.frontend.auth.register');
-                Route::post('/' . Translation::get('register-slug', 'slug', 'register'), [AuthController::class, 'registerPost'])->name('qcommerce.frontend.auth.register.post');
+        Route::get('/' . Translation::get('logout-slug', 'slug', 'logout'), [AuthController::class, 'logout'])->name('qcommerce.frontend.auth.logout');
+        Route::group([
+            'middleware' => [GuestMiddleware::class],
+        ], function () {
+            Route::get('/' . Translation::get('login-slug', 'slug', 'login'), [AuthController::class, 'login'])->name('qcommerce.frontend.auth.login');
+            Route::post('/' . Translation::get('login-slug', 'slug', 'login'), [AuthController::class, 'loginPost'])->name('qcommerce.frontend.auth.login.post');
+            Route::post('/' . Translation::get('register-slug', 'slug', 'register'), [AuthController::class, 'login'])->name('qcommerce.frontend.auth.register');
+            Route::post('/' . Translation::get('register-slug', 'slug', 'register'), [AuthController::class, 'registerPost'])->name('qcommerce.frontend.auth.register.post');
 
-                Route::get('/' . Translation::get('forgot-password-slug', 'slug', 'forgot-password'), [AuthController::class, 'forgotPassword'])->name('qcommerce.frontend.auth.forgot-password');
-                Route::post('/' . Translation::get('forgot-password-slug', 'slug', 'forgot-password'), [AuthController::class, 'forgotPasswordPost'])->name('qcommerce.frontend.auth.forgot-password.post');
-                Route::get('/' . Translation::get('reset-password-slug', 'slug', 'reset-password') . '/{passwordResetToken}', [AuthController::class, 'resetPassword'])->name('qcommerce.frontend.auth.reset-password');
-                Route::post('/' . Translation::get('reset-password-slug', 'slug', 'reset-password') . '/{passwordResetToken}', [AuthController::class, 'resetPasswordPost'])->name('qcommerce.frontend.auth.reset-password.post');
-            });
+            Route::get('/' . Translation::get('forgot-password-slug', 'slug', 'forgot-password'), [AuthController::class, 'forgotPassword'])->name('qcommerce.frontend.auth.forgot-password');
+            Route::post('/' . Translation::get('forgot-password-slug', 'slug', 'forgot-password'), [AuthController::class, 'forgotPasswordPost'])->name('qcommerce.frontend.auth.forgot-password.post');
+            Route::get('/' . Translation::get('reset-password-slug', 'slug', 'reset-password') . '/{passwordResetToken}', [AuthController::class, 'resetPassword'])->name('qcommerce.frontend.auth.reset-password');
+            Route::post('/' . Translation::get('reset-password-slug', 'slug', 'reset-password') . '/{passwordResetToken}', [AuthController::class, 'resetPasswordPost'])->name('qcommerce.frontend.auth.reset-password.post');
+        });
 
-            Route::group([
-                'middleware' => [AuthMiddleware::class],
-            ], function () {
-                //Account routes
-                Route::prefix('/' . Translation::get('account-slug', 'slug', 'account'))->group(function () {
-                    Route::get('/', [AccountController::class, 'account'])->name('qcommerce.frontend.account');
-                    Route::post('/', [AccountController::class, 'accountPost'])->name('qcommerce.frontend.account.post');
-                });
+        Route::group([
+            'middleware' => [AuthMiddleware::class],
+        ], function () {
+            //Account routes
+            Route::prefix('/' . Translation::get('account-slug', 'slug', 'account'))->group(function () {
+                Route::get('/', [AccountController::class, 'account'])->name('qcommerce.frontend.account');
+                Route::post('/', [AccountController::class, 'accountPost'])->name('qcommerce.frontend.account.post');
             });
-        }
+        });
     }
 );
 
