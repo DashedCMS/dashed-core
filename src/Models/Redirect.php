1331d72d4d8a6b36c6e7fe99b62bff26d71412d3
Robin van Maasakker <robin@qubiqx.com>
2023-08-08 08:24:33 +0200

    Update

--- src/Models/Redirect.php
+++ src/Models/Redirect.php
@@ -11,14 +11,11 @@ class Redirect extends Model
 
     protected $table = 'dashed__redirects';
 
-    protected $dates = [
-        'created_at',
-        'updated_at',
-        'deleted_at',
-    ];
-
     protected $casts = [
         'delete_redirect_after' => 'date',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'deleted_at' => 'datetime',
     ];
 
     public static function handleSlugChange(?string $oldSlug, ?string $newSlug)
