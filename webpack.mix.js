50476a42d4033acd465bb60625b51eeac7adc890
Robin van Maasakker <robin@qubiqx.com>
2023-08-09 16:16:43 +0200

    Update

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require("laravel-mix");
 
-mix.postCss('./resources/css/filament.css', './resources/dist/css', [
+mix.postCss('./resources/css/filament.css', './resources/dist', [
     require('tailwindcss', './tailwind.config.js'),
 ])
