115498c89cd524798ee7844783b5c7a85723ad16
Robin van Maasakker <robin@qubiqx.com>
2023-08-09 15:42:29 +0200

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,15 @@ export default {
     safelist: [
         'bg-red-100',
         'text-red-800',
+        'bg-blue-100',
+        'text-blue-800',
+        'bg-purple-100',
+        'text-purple-800',
+        'bg-yellow-100',
+        'text-yellow-800',
+        'bg-green-100',
+        'text-green-800',
+        'line-through',
     ],
     theme: {
         extend: {
