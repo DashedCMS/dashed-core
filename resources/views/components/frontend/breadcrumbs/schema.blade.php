77b1dca4f47590362e8af8bdd3bd903d1dc37bcb
Robin van Maasakker <robin@qubiqx.com>
2024-05-03 14:12:30 +0200

    Update

--- resources/views/components/frontend/breadcrumbs/schema.blade.php
+++ resources/views/components/frontend/breadcrumbs/schema.blade.php
@@ -5,6 +5,9 @@
         foreach($breadcrumbs as $breadcrumb){
             $items[] = \Spatie\SchemaOrg\Schema::listItem()
                 ->position($loop)
+                ->url($breadcrumb['url'])
+                ->name($breadcrumb['name'])
+                ->identifier($breadcrumb['name'])
                 ->item(\Spatie\SchemaOrg\Schema::webPage()->url($breadcrumb['url'])->name($breadcrumb['name']));
             $loop++;
         }
