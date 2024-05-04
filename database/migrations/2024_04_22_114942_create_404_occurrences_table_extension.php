3798791a514449a7feddd68bcc4b4dd14f6c1836
Robin van Maasakker <robin@qubiqx.com>
2024-05-04 13:09:48 +0200

    Update

---
+++ database/migrations/2024_04_22_114942_create_404_occurrences_table_extension.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('dashed__not_found_pages', function (Blueprint $table) {
+            $table->text('link')
+                ->change();
+        });
+
+        Schema::table('dashed__not_found_page_occurrences', function (Blueprint $table) {
+            $table->text('referer')
+                ->change()
+                ->nullable();
+            $table->text('user_agent')
+                ->change()
+                ->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('404_occurrences');
+    }
+};
