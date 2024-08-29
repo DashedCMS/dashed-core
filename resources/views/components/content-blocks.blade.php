a8a2f323e5c46ae6f275847743a22a0d36ca36ab
Robin van Maasakker <robin@qubiqx.com>
2024-08-29 16:15:30 +0200

    Update

--- resources/views/components/content-blocks.blade.php
+++ resources/views/components/content-blocks.blade.php
@@ -1,9 +1,12 @@
-@php($attributes = $attributes->except('content'))
 @if($content)
     @foreach($content as $block)
-        @cache("block_{$loop->iteration}_{$model->id}_{$model->updated_at}")
-        <x-dynamic-component :component="'blocks.' . $block['type']" :type="$block['type']" :data="$block['data']"
-            {{ $attributes->merge() }} />
-        @endcache
+        @if(in_array($block['type'], config('dashed-core.blocks.caching_disabled')))
+            <x-dynamic-component :component="'blocks.' . $block['type']" :type="$block['type']" :data="$block['data']"/>
+        @else
+            @cache("block_{$loop->iteration}_{$model->id}_{$model->updated_at}")
+            <x-dynamic-component :component="'blocks.' . $block['type']" :type="$block['type']"
+                                 :data="$block['data']"></x-dynamic-component>
+            @endcache
+        @endif
     @endforeach
 @endif
