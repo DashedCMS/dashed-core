7ed32d997af59b5bd23cc4a028206a025f114eda
Robin van Maasakker <robin@qubiqx.com>
2024-08-29 15:41:20 +0200

    Update

--- resources/views/components/content-blocks.blade.php
+++ resources/views/components/content-blocks.blade.php
@@ -1,8 +1,9 @@
+@php($attributes = $attributes->except('content'))
 @if($content)
     @foreach($content as $block)
         @cache("block_{$loop->iteration}_{$model->id}_{$model->updated_at}")
-        <x-dynamic-component :component="'blocks.' . $block['type']" :type="$block['type']"
-                             :data="$block['data']"></x-dynamic-component>
+        <x-dynamic-component :component="'blocks.' . $block['type']" :type="$block['type']" :data="$block['data']"
+            {{ $attributes->merge() }} />
         @endcache
     @endforeach
 @endif
