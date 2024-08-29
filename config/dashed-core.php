b9ce8f0b81b04a58f7b07f5ae20a9c235e6197b2
Robin van Maasakker <robin@qubiqx.com>
2024-08-29 16:21:52 +0200

    Update

--- config/dashed-core.php
+++ config/dashed-core.php
@@ -3,4 +3,10 @@
 return [
     'show_default_user_resource' => true,
     'default_auth_pages_enabled' => true,
+
+    'blocks' => [
+        'caching_disabled' => [
+            'example-block'
+        ]
+    ]
 ];
--- resources/views/components/content-blocks.blade.php
+++ resources/views/components/content-blocks.blade.php
@@ -1,11 +1,11 @@
 @if($content)
     @foreach($content as $block)
-        @if(in_array($block['type'], config('dashed-core.blocks.caching_disabled')))
-            <x-dynamic-component :component="'blocks.' . $block['type']" :type="$block['type']" :data="$block['data']"/>
+        @if(in_array($block['type'], config('dashed-core.blocks.caching_disabled', [])))
+            <x-dynamic-component :component="'blocks.' . $block['type']" :type="$block['type']" :data="$block['data']" {{ $attributes->merge() }}/>
         @else
             @cache("block_{$loop->iteration}_{$model->id}_{$model->updated_at}")
             <x-dynamic-component :component="'blocks.' . $block['type']" :type="$block['type']"
-                                 :data="$block['data']"></x-dynamic-component>
+                                 :data="$block['data']" {{ $attributes->merge() }}></x-dynamic-component>
             @endcache
         @endif
     @endforeach
