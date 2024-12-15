ee99c57b216f7cdb0265916d98b0a75f6e5512c3
Robinvm <Robinvm@users.noreply.github.com>
2024-12-15 19:28:24 +0000

    Styling fixed with PHP CS Fixer

--- src/Middleware/GuestMiddleware.php
+++ src/Middleware/GuestMiddleware.php
@@ -4,7 +4,6 @@ namespace Dashed\DashedCore\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Dashed\DashedCore\Classes\AccountHelper;
 
 class GuestMiddleware
