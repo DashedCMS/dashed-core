4a8fb87ddadd4ddf17616bac933cb8e073d13cc3
Robinvm <Robinvm@users.noreply.github.com>
2024-12-15 19:18:45 +0000

    Styling fixed with PHP CS Fixer

--- src/Livewire/Frontend/Auth/Login.php
+++ src/Livewire/Frontend/Auth/Login.php
@@ -2,12 +2,11 @@
 
 namespace Dashed\DashedCore\Livewire\Frontend\Auth;
 
-use Dashed\DashedCore\Classes\AccountHelper;
-use Dashed\DashedCore\Livewire\Frontend\Account\Account;
 use Livewire\Component;
 use Dashed\DashedCore\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Dashed\DashedCore\Classes\AccountHelper;
 use Dashed\DashedTranslations\Models\Translation;
 use Dashed\DashedEcommerceCore\Classes\ShoppingCart;
 
