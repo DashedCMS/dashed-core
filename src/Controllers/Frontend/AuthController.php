684efa7a2efd895825de400c37de03d8824231dd
Robin van Maasakker <robin@qubiqx.com>
2024-12-18 08:15:15 +0100

    Update

--- src/Controllers/Frontend/AuthController.php
+++ src/Controllers/Frontend/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace Dashed\DashedCore\Controllers\Frontend;
 
+use Dashed\DashedCore\Classes\AccountHelper;
 use Exception;
 use Carbon\Carbon;
 use Illuminate\Support\Str;
@@ -28,7 +29,7 @@ class AuthController extends FrontendController
             return redirect(route('dashed.frontend.account'))->with('success', Translation::get('already-logged-in', 'login', 'You are already logged in'));
         }
 
-        if (View::exists(env('SITE_THEME', 'dashed').'.auth.login')) {
+        if (View::exists(env('SITE_THEME', 'dashed') . '.auth.login')) {
             seo()->metaData('metaTitle', Translation::get('login-page-meta-title', 'login', 'Login'));
             seo()->metaData('metaDescription', Translation::get('login-page-meta-description', 'login', 'Login to your account'));
 
@@ -36,56 +37,21 @@ class AuthController extends FrontendController
                 'livewireComponent' => Login::class,
             ]);
 
-            return view(env('SITE_THEME', 'dashed').'.auth.login');
+            return view(env('SITE_THEME', 'dashed') . '.auth.login');
         } else {
             return $this->pageNotFound();
         }
     }
 
-    public function loginPost(LoginRequest $request)
-    {
-        throw new Exception('The loginPost method is outdated, use Livewire instead');
-
-        $user = User::where('email', $request->email)->first();
-
-        if (! $user) {
-            return redirect()->back()->with('error', Translation::get('no-user-found', 'login', 'We could not find a user matching these criteria'));
-        }
-
-        if (! Hash::check($request->password, $user->password)) {
-            return redirect()->back()->with('error', Translation::get('no-user-found', 'login', 'We could not find a user matching these criteria'));
-        }
-
-        Auth::login($user, $request->remember_me);
-
-        return redirect(route('dashed.frontend.account'))->with('success', Translation::get('succesfully-logged-in', 'login', 'You are logged in!'));
-    }
-
-    public function registerPost(RegisterRequest $request)
-    {
-        throw new Exception('The registerPost method is outdated, use Livewire instead');
-
-        $user = new User();
-        $user->first_name = $request->first_name;
-        $user->last_name = $request->last_name;
-        $user->email = $request->email;
-        $user->password = Hash::make($request->password);
-        $user->save();
-
-        Auth::login($user, $request->remember_me);
-
-        return redirect(route('dashed.frontend.account'))->with('success', Translation::get('succesfully-registered', 'login', 'You are registered!'));
-    }
-
     public function logout()
     {
-        if (! Auth::check()) {
-            return redirect(route('dashed.frontend.auth.login'))->with('success', Translation::get('already-logged-out', 'login', 'You are already logged out'));
+        if (!Auth::check()) {
+            return redirect(AccountHelper::getLoginUrl())->with('success', Translation::get('already-logged-out', 'login', 'You are already logged out'));
         }
 
         Auth::logout();
 
-        return redirect(route('dashed.frontend.auth.login'))->with('success', Translation::get('succesfully-logged-out', 'login', 'You are logged out!'));
+        return redirect(AccountHelper::getLoginUrl())->with('success', Translation::get('succesfully-logged-out', 'login', 'You are logged out!'));
     }
 
     public function forgotPassword()
@@ -94,7 +60,7 @@ class AuthController extends FrontendController
             return redirect(route('dashed.frontend.account'))->with('success', Translation::get('already-logged-in', 'login', 'You are already logged in'));
         }
 
-        if (View::exists(env('SITE_THEME', 'dashed').'.auth.forgot-password')) {
+        if (View::exists(env('SITE_THEME', 'dashed') . '.auth.forgot-password')) {
             seo()->metaData('metaTitle', Translation::get('forgot-password-page-meta-title', 'login', 'Forgot password'));
             seo()->metaData('metaDescription', Translation::get('forgot-password-page-meta-description', 'login', 'Forgot your password?'));
 
@@ -102,29 +68,10 @@ class AuthController extends FrontendController
                 'livewireComponent' => ForgotPassword::class,
             ]);
 
-            return view(env('SITE_THEME', 'dashed').'.auth.forgot-password');
+            return view(env('SITE_THEME', 'dashed') . '.auth.forgot-password');
         } else {
             return $this->pageNotFound();
         }
-    }
-
-    public function forgotPasswordPost(ForgotPasswordRequest $request)
-    {
-        throw new Exception('The forgotPasswordPost method is outdated, use Livewire instead');
-
-        if (Auth::check()) {
-            return redirect(route('dashed.frontend.account'))->with('success', Translation::get('already-logged-in', 'login', 'You are already logged in'));
-        }
-
-        $user = User::where('email', $request->email)->first();
-        if ($user) {
-            $user->password_reset_token = Str::random(64);
-            $user->password_reset_requested = Carbon::now();
-            $user->save();
-            Mail::to($user->email)->send(new PasswordResetMail($user));
-        }
-
-        return redirect()->back()->with('success', Translation::get('forgot-password-post-success', 'login', 'If we can find an account with your email you will receive a email to reset your password.'));
     }
 
     public function resetPassword($passwordResetToken)
@@ -133,13 +80,13 @@ class AuthController extends FrontendController
             return redirect(route('dashed.frontend.account'))->with('success', Translation::get('already-logged-in', 'login', 'You are already logged in'));
         }
 
-        if (View::exists(env('SITE_THEME', 'dashed').'.auth.reset-password')) {
+        if (View::exists(env('SITE_THEME', 'dashed') . '.auth.reset-password')) {
             seo()->metaData('metaTitle', Translation::get('reset-password-page-meta-title', 'login', 'Reset password'));
             seo()->metaData('metaDescription', Translation::get('reset-password-page-meta-description', 'login', 'Reset your password'));
 
             $user = User::where('password_reset_token', $passwordResetToken)->first();
 
-            if (! $user) {
+            if (!$user) {
                 return redirect(route('dashed.frontend.auth.forgot-password'))->with('success', Translation::get('reset-token-invalid', 'login', 'The token that was provided is invalid'));
             }
 
@@ -154,33 +101,9 @@ class AuthController extends FrontendController
                 ],
             ]);
 
-            return view(env('SITE_THEME', 'dashed').'.auth.reset-password');
+            return view(env('SITE_THEME', 'dashed') . '.auth.reset-password');
         } else {
             return $this->pageNotFound();
         }
-    }
-
-    public function resetPasswordPost(ResetPasswordRequest $request, $passwordResetToken)
-    {
-        throw new Exception('The resetPasswordPost method is outdated, use Livewire instead');
-
-        if (Auth::check()) {
-            return redirect(route('dashed.frontend.account'))->with('success', Translation::get('already-logged-in', 'login', 'You are already logged in'));
-        }
-
-        $user = User::where('password_reset_token', $passwordResetToken)->first();
-
-        if (! $user) {
-            return redirect(route('dashed.frontend.auth.forgot-password'))->with('success', Translation::get('reset-token-invalid', 'login', 'The token that was provided is invalid'));
-        }
-
-        $user->password_reset_token = null;
-        $user->password_reset_requested = null;
-        $user->password = Hash::make($request->password);
-        $user->save();
-
-        Auth::login($user);
-
-        return redirect(route('dashed.frontend.account'))->with('success', Translation::get('reset-password-post-success', 'login', 'Your password has been reset!'));
     }
 }
