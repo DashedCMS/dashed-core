ff92d7d3d71b4f40d9b35cb8e29cf255e96776ab
Robin van Maasakker <robin@dashed.nl>
2025-12-19 14:36:54 +0100

    Update

---
+++ src/Jobs/GenerateAIContent.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Dashed\DashedCore\Jobs;
+
+use Dashed\DashedCore\Classes\OpenAIHelper;
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\DB;
+use Dashed\DashedCore\Classes\Locales;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Dashed\DashedEcommerceCore\Models\Product;
+use Dashed\DashedTranslations\Models\Translation;
+use Dashed\DashedEcommerceCore\Models\ProductGroup;
+use Dashed\DashedEcommerceCore\Models\ProductFilterOption;
+
+class GenerateAIContent implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    public $tries = 5;
+    public $timeout = 1200;
+
+    public $model;
+    public string $column;
+    public string $content;
+    public string $locale;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($model, string $column, string $content, string $locale)
+    {
+        $this->model = $model;
+        $this->column = $column;
+        $this->content = $content;
+        $this->locale = $locale;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $description = OpenAIHelper::runPrompt(prompt: $this->content);
+        $this->model->setTranslation($this->column, $this->locale, $description);
+        $this->model->saveQuietly();
+    }
+}
