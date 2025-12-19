0a40e2f904614e197960fda6c1996398a06fb3c2
Robin van Maasakker <robin@dashed.nl>
2025-12-19 10:49:03 +0100

    Update

--- src/Filament/Resources/UserResource/Users/ListUsers.php
+++ src/Filament/Resources/UserResource/Users/ListUsers.php
@@ -2,9 +2,14 @@
 
 namespace Dashed\DashedCore\Filament\Resources\UserResource\Users;
 
+use Dashed\DashedCore\Mail\NewAdminAccountMail;
+use Filament\Actions\Action;
 use Filament\Actions\CreateAction;
+use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
 use Dashed\DashedCore\Filament\Resources\UserResource;
+use Illuminate\Support\Facades\Mail;
 
 class ListUsers extends ListRecords
 {
@@ -14,6 +19,46 @@ class ListUsers extends ListRecords
     {
         return [
             CreateAction::make(),
+            Action::make('createAdminUser')
+                ->label('Admin user aanmaken')
+                ->button()
+                ->schema([
+                    TextInput::make('first_name')
+                        ->required()
+                        ->label('Voornaam'),
+                    TextInput::make('last_name')
+                        ->required()
+                        ->label('Achternaam'),
+                    TextInput::make('email')
+                        ->required()
+                        ->email()
+                        ->label('E-mail'),
+                ])
+                ->action(function (array $data): void {
+                    $password = bin2hex(random_bytes(8));
+                    $data['password'] = $password;
+                    $user = \Dashed\DashedCore\Models\User::create([
+                        'first_name' => $data['first_name'],
+                        'last_name' => $data['last_name'],
+                        'email' => $data['email'],
+                        'password' => bcrypt($data['password']),
+                        'role' => 'admin',
+                    ]);
+
+                    try{
+                       Mail::to($user->email)->send(new NewAdminAccountMail($user, $password));
+                    }catch (\Exception $exception){
+                        Notification::make()
+                            ->title('Fout bij het verzenden van de e-mail: ' . $exception->getMessage())
+                            ->danger()
+                            ->send();
+                    }
+
+                    Notification::make()
+                        ->title('Admin gebruiker ' . $user->first_name . ' ' . $user->last_name . ' is aangemaakt.')
+                        ->success()
+                        ->send();
+                }),
         ];
     }
 }
---
+++ src/Mail/NewAdminAccountMail.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Dashed\DashedCore\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Dashed\DashedCore\Models\User;
+use Illuminate\Queue\SerializesModels;
+use Dashed\DashedCore\Models\Customsetting;
+use Dashed\DashedTranslations\Models\Translation;
+
+class NewAdminAccountMail extends Mailable
+{
+    use Queueable;
+    use SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user, string $password)
+    {
+        $this->user = $user;
+        $this->password = $password;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $view = view()->exists(config('dashed-core.site_theme', 'dashed') . '.emails.new-admin-account') ? config('dashed-core.site_theme', 'dashed') . '.emails.new-admin-account' : 'dashed-core::emails.new-admin-account';
+
+        return $this->view($view)
+            ->from(Customsetting::get('site_from_email'), Customsetting::get('site_name'))->subject(Translation::get('new-admin-account-email-subject', 'login', 'Je admin account bij :siteName: is aangemaakt.', 'text', [
+                'siteName' => Customsetting::get('site_name'),
+            ]))
+            ->with([
+                'user' => $this->user,
+                'password' => $this->password,
+            ]);
+    }
+}
