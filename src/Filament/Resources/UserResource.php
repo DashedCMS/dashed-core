7aeb19fdfb768dcb90cf08cd377605e279c72fa7
Robin van Maasakker <robin@dashed.nl>
2026-02-26 09:24:48 +0100

    Update

--- src/Filament/Resources/UserResource.php
+++ src/Filament/Resources/UserResource.php
@@ -2,6 +2,7 @@
 
 namespace Dashed\DashedCore\Filament\Resources;
 
+use Filament\Forms\Components\Toggle;
 use UnitEnum;
 use BackedEnum;
 use App\Models\User;
@@ -62,16 +63,19 @@ class UserResource extends Resource
                             ->label('Voornaam')
                             ->required()
                             ->maxLength(255),
+
                         TextInput::make('last_name')
                             ->label('Achternaam')
                             ->required()
                             ->maxLength(255),
+
                         TextInput::make('email')
                             ->label('Email')
                             ->unique('users', 'email', fn ($record) => $record)
                             ->required()
                             ->email()
                             ->maxLength(255),
+
                         Select::make('role')
                             ->label('Rol')
                             ->required()
@@ -79,6 +83,7 @@ class UserResource extends Resource
                                 'customer' => 'Customer',
                                 'admin' => 'Admin',
                             ]),
+
                         TextInput::make('password')
                             ->label('Wachtwoord')
                             ->nullable()
@@ -89,6 +94,7 @@ class UserResource extends Resource
                             ->required(fn ($livewire) => $livewire instanceof CreateUser)
                             ->helperText('Het wachtwoord wordt alleen aangepast als je iets invult')
                             ->reactive(),
+
                         TextInput::make('password_confirmation')
                             ->label('Wachtwoord herhalen')
                             ->required(fn (Get $get) => $get('password'))
@@ -96,6 +102,103 @@ class UserResource extends Resource
                             ->minLength(6)
                             ->maxLength(255)
                             ->reactive(),
+                    ])
+                    ->columns(2),
+
+                Section::make('Contact')->columnSpanFull()
+                    ->schema([
+                        TextInput::make('phone_number')
+                            ->label('Telefoonnummer')
+                            ->tel()
+                            ->maxLength(32),
+
+                        TextInput::make('date_of_birth')
+                            ->label('Geboortedatum')
+                            ->type('date'),
+
+                        Select::make('gender')
+                            ->label('Geslacht')
+                            ->options([
+                                'm' => 'Man',
+                                'f' => 'Vrouw',
+                            ])
+                            ->nullable(),
+
+                        Select::make('marketing')
+                            ->label('Nieuwsbrief')
+                            ->options([
+                                0 => 'Nee',
+                                1 => 'Ja',
+                            ])
+                            ->default(0),
+                    ])
+                    ->columns(2),
+
+                Section::make('Verzendadres')->columnSpanFull()
+                    ->schema([
+                        TextInput::make('street')
+                            ->label('Straat')
+                            ->maxLength(255),
+
+                        TextInput::make('house_nr')
+                            ->label('Huisnummer')
+                            ->maxLength(50),
+
+                        TextInput::make('zip_code')
+                            ->label('Postcode')
+                            ->maxLength(50),
+
+                        TextInput::make('city')
+                            ->label('Stad')
+                            ->maxLength(255),
+
+                        TextInput::make('country')
+                            ->label('Land')
+                            ->maxLength(255),
+                    ])
+                    ->columns(2),
+
+                Section::make('Bedrijf')->columnSpanFull()
+                    ->schema([
+                        Toggle::make('is_company')
+                            ->label('Bestelt als bedrijf')
+                            ->default(0)
+                            ->columnSpanFull()
+                            ->reactive(),
+
+                        TextInput::make('company')
+                            ->label('Bedrijfsnaam')
+                            ->maxLength(255)
+                            ->visible(fn (Get $get) => (bool) $get('is_company')),
+
+                        TextInput::make('tax_id')
+                            ->label('BTW ID')
+                            ->maxLength(255)
+                            ->visible(fn (Get $get) => (bool) $get('is_company')),
+                    ])
+                    ->columns(2),
+
+                Section::make('Factuuradres')->columnSpanFull()
+                    ->schema([
+                        TextInput::make('invoice_street')
+                            ->label('Straat')
+                            ->maxLength(255),
+
+                        TextInput::make('invoice_house_nr')
+                            ->label('Huisnummer')
+                            ->maxLength(50),
+
+                        TextInput::make('invoice_zip_code')
+                            ->label('Postcode')
+                            ->maxLength(50),
+
+                        TextInput::make('invoice_city')
+                            ->label('Stad')
+                            ->maxLength(255),
+
+                        TextInput::make('invoice_country')
+                            ->label('Land')
+                            ->maxLength(255),
                     ])
                     ->columns(2),
             ]);
