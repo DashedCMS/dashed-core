f8a516da94871511059fe26f7f1923b81eb7c43e
Robin van Maasakker <robin@dashed.nl>
2026-04-22 12:02:10 +0200

    feat(structured-data): custom_structured_data table

---
+++ database/migrations/2026_04_22_100010_create_custom_structured_data_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('dashed__custom_structured_data', function (Blueprint $table) {
+            $table->id();
+            $table->string('subject_type');
+            $table->unsignedBigInteger('subject_id');
+            $table->string('schema_type');
+            $table->longText('json_ld');
+            $table->integer('sort_order')->default(0);
+            $table->timestamps();
+
+            $table->unique(
+                ['subject_type', 'subject_id', 'schema_type'],
+                'custom_structured_data_unique'
+            );
+            $table->index(['subject_type', 'subject_id'], 'custom_structured_data_subject_idx');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('dashed__custom_structured_data');
+    }
+};
