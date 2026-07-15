414765bbc71328094872b97e3ac60eddc5d6484b
Robin van Maasakker <robin@dashed.nl>
2026-07-15 22:47:18 +0200

    feat(mail-log): add secured postmark webhook endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,9 @@
 <?php
 
 // API-routes voor dashed-core.
+
+use Illuminate\Support\Facades\Route;
+use Dashed\DashedCore\Http\Controllers\PostmarkWebhookController;
+
+Route::post('/dashed/webhooks/postmark', PostmarkWebhookController::class)
+    ->name('dashed.webhooks.postmark');
---
+++ src/Http/Controllers/PostmarkWebhookController.php
@@ -0,0 +1,38 @@
+<?php
+
+// packages/dashed/dashed-core/src/Http/Controllers/PostmarkWebhookController.php
+
+namespace Dashed\DashedCore\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Dashed\DashedCore\Mail\PostmarkEventHandler;
+
+class PostmarkWebhookController extends Controller
+{
+    public function __invoke(Request $request, PostmarkEventHandler $handler): JsonResponse
+    {
+        $secret = config('dashed-core.sent_emails.postmark_webhook_secret');
+
+        if ($secret && ! $this->authorized($request, $secret)) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        $handler->handle($request->all());
+
+        return response()->json(['message' => 'ok'], 200);
+    }
+
+    protected function authorized(Request $request, string $secret): bool
+    {
+        // Postmark stuurt basic-auth; de query-param is een fallback voor tests
+        // en simpele setups.
+        if (hash_equals($secret, (string) $request->query('secret', ''))) {
+            return true;
+        }
+
+        return hash_equals($secret, (string) $request->getPassword())
+            || hash_equals($secret, (string) $request->getUser());
+    }
+}
