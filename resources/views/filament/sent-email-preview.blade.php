3ed7bf391480c9235ba43340cad3c17bc99162de
Robin van Maasakker <robin@dashed.nl>
2026-07-15 23:05:37 +0200

    feat(mail-log): html preview and status timeline on view page

---
+++ resources/views/filament/sent-email-preview.blade.php
@@ -0,0 +1,6 @@
+<iframe
+    sandbox=""
+    class="w-full"
+    style="height: 600px; border: 1px solid #e5e7eb; border-radius: 0.5rem; background: #fff;"
+    srcdoc="{{ $record->html_body }}"
+></iframe>
--- src/Filament/Resources/SentEmailResource/Pages/ViewSentEmail.php
+++ src/Filament/Resources/SentEmailResource/Pages/ViewSentEmail.php
@@ -2,7 +2,11 @@
 
 namespace Dashed\DashedCore\Filament\Resources\SentEmailResource\Pages;
 
+use Filament\Schemas\Schema;
 use Filament\Resources\Pages\ViewRecord;
+use Filament\Schemas\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Components\ViewEntry;
 use Dashed\DashedCore\Filament\Resources\SentEmailResource;
 
 class ViewSentEmail extends ViewRecord
@@ -12,5 +16,53 @@ class ViewSentEmail extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [];
+    }
+
+    public function infolist(Schema $schema): Schema
+    {
+        return $schema->components([
+            Section::make('Details')
+                ->schema([
+                    TextEntry::make('to_email')
+                        ->label('Ontvanger'),
+                    TextEntry::make('subject')
+                        ->label('Onderwerp'),
+                    TextEntry::make('status')
+                        ->label('Status')
+                        ->badge(),
+                    TextEntry::make('created_at')
+                        ->label('Verzonden')
+                        ->dateTime(),
+                    TextEntry::make('delivered_at')
+                        ->label('Afgeleverd')
+                        ->dateTime()
+                        ->placeholder('-'),
+                    TextEntry::make('opened_at')
+                        ->label('Geopend')
+                        ->dateTime()
+                        ->placeholder('-'),
+                    TextEntry::make('open_count')
+                        ->label('Aantal opens'),
+                    TextEntry::make('clicked_at')
+                        ->label('Geklikt')
+                        ->dateTime()
+                        ->placeholder('-'),
+                    TextEntry::make('click_count')
+                        ->label('Aantal clicks'),
+                    TextEntry::make('bounced_at')
+                        ->label('Gebounced')
+                        ->dateTime()
+                        ->placeholder('-'),
+                    TextEntry::make('bounce_reason')
+                        ->label('Bounce-reden')
+                        ->placeholder('-'),
+                ])
+                ->columns(2),
+            Section::make('Preview')
+                ->schema([
+                    ViewEntry::make('html_body')
+                        ->view('dashed-core::filament.sent-email-preview'),
+                ]),
+        ]);
     }
 }
